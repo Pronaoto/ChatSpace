85575e281938152aad442d94e2b716dfb96174f3
shoji_naoto <onyxnaoto@gmail.com>
2018-09-04 14:23:35 +0900

    0904-1
    jsファイルの編集
    ビューの編集
    jbuilderの編集

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -1,83 +1,70 @@
+$(document).on('turbolinks:load', function(){
 $(function() {
+  var search_list = $("#user-search-result");
+  var member_list = $("#chat-group-users");
 
-var search_list = $(".chat-group-form__field clearfix");
-
-function appendUser(user){
-  var html = `<div class="chat-group-user clearfix">
-  <p class="chat-group-user__name">${user.name}</p>
-  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
-  </div>`
-    search_list.append(html);
-}
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${user.name}</p>
+    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+    </div>`;
+      search_list.append(html);
+  }
   function appendNoUser(user){
-    var html = `<div class='chat-group-users js-add-user'>`
+    var html = `<div class='chat-group-user clearfix'>${ user }</div>`
     search_list.append(html);
   }
+
+  function addUser(userId,userName) {
+  var html = `<div id='chat-group-users'>
+              <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+              <input name='group[user_ids][]' type='hidden' value='${userId}'>
+              <p class='chat-group-user__name'>${userName}</p>
+              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`;
+  member_list.append(html);
+  // console.log("ok");
+    }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     // console.log(input);
     $.ajax({
       type: 'GET',
-      url: '/groups/new',
+      url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
     .done(function(users){
       // console.log(users);
-      $(".chat-group-form__field clearfix").empty();
+      $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
         });
       }
       else {
-        appendNoUser("ユーザー検索に失敗しました");
+        appendNoUser("一致するユーザはいません");
       }
     })
-    .fail(function() {
-      alert('error');
+    .fail(function(){
+      alert('検索に失敗しました');
     })
+  });
+    $(document).on("click", ".user-search-add", function () {
+    $('#chat-group-users').val();
+      var userId = $(this).data('user-id');
+      var userName = $(this).data('user-name');
+      // console.log(this);
+      addUser(userId,userName);
+      $(this).parent().remove();
+    });
+
+    $(document).on("click", ".user-search-remove", function () {
+      $(this).parent().remove();
+      // console.log(this);
+    });
   });
 });
 
 
-// $(document).on('turbolinks:load', function() {
-//   function buildHTML(user){
-//     var html =
-//               `.chat-group-form__field--left
-//                 %label.chat-group-form__label{:for => "chat_group_チャットメンバーを追加"} チャットメンバーを追加
-//                 .chat-group-form__field--right
-//                 .chat-group-form__search.clearfix
-//                 %input#user-search-field.chat-group-form__input{:placeholder => "追加したいユーザー名を入力してください", :type => "text"}/
-//                 .user-search-result`
-//       return html;
-//     }
-
-//   $('.chat-group-form__search.clearfix').on('submit', function(e){
-//     e.preventDefault();
-//     console.log($(this));
-
-//     var formData = new FormData($(this).get(0));
-//     // for(item of formData) { console.log(item); }
-//     var url = $(this).attr('action');
-//     $.ajax({
-//       url: url,
-//       type: 'POST',
-//       data: formData,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var html = buildHTML(data);
-//       $('.chatroom__body').append(html).animate({scrollTop: $('.chatroom__body')[0].scrollHeight}, '500');
-//       $('#message_content').val('');
-//       $('input').prop('disabled', false);
-//     })
-//     .fail(function(){
-//       alert('ユーザー検索に失敗しました');
-//     });
-//     //return false;
-//   });
-// });
