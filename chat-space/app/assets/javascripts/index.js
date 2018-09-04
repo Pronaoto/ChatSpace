bb6175bfe873ae62fc9027cbc7eb9c8427647d5d
shoji_naoto <onyxnaoto@gmail.com>
2018-09-04 14:55:08 +0900

    edit-index
    インデントの調整。

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -1,26 +1,29 @@
 $(document).on('turbolinks:load', function(){
 $(function() {
+
   var search_list = $("#user-search-result");
-  var member_list = $("#chat-group-users");
 
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
-    <p class="chat-group-user__name">${user.name}</p>
-    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
-    </div>`;
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`;
       search_list.append(html);
   }
+
   function appendNoUser(user){
     var html = `<div class='chat-group-user clearfix'>${ user }</div>`
     search_list.append(html);
   }
 
+  var member_list = $("#chat-group-users");
+
   function addUser(userId,userName) {
   var html = `<div id='chat-group-users'>
-              <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
-              <input name='group[user_ids][]' type='hidden' value='${userId}'>
-              <p class='chat-group-user__name'>${userName}</p>
-              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                    <p class='chat-group-user__name'>${userName}</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
               </div>`;
   member_list.append(html);
   // console.log("ok");
